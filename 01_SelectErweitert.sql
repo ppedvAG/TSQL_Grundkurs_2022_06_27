1995d7e1535ad0a43f128195a445d8a8427448a1
ppedvLukasK <lukask@ppedv.de>
2022-06-27 11:05:20 +0200

    M001

---
+++ 01_SelectErweitert.sql
@@ -0,0 +1,9 @@
+SELECT Freight, Freight % 5 FROM Orders; --%: Modulo, Rest einer Division
+
+SELECT CompanyName, (Address + ' ' + City + ', ' + PostalCode + ' ' + Country) AS VolleAddresse FROM Customers; --mit + Strings (Zeichenketten) verbindet sie miteinander
+
+SELECT DISTINCT Country FROM Customers; --Datensätze einzigartig machen
+
+SELECT c.Country FROM Customers c; --c als Tabellenalias, bei mehreren Tabellen mit gleichen Spalten, wichtig bei Subselects
+
+SELECT * FROM Customers ORDER BY Country ASC; --ORDER BY: Sortiere nach einer Spalte, ASC/DESC gibt die Richtung an
